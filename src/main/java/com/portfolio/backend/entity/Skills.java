738c37f558c4c99ff832ff9dddacb336aabffce9
MarieM <ingmariemelendez@gmail.com>
2023-02-18 17:45:03 -0300

    correcciones para render

--- src/main/java/com/portfolio/backend/entity/Skills.java
+++ src/main/java/com/portfolio/backend/entity/Skills.java
@@ -1,11 +1,9 @@
 package com.portfolio.backend.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
 import lombok.Getter;
 import lombok.Setter;
 
